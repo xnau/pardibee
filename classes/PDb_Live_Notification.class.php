7865ffebaf370e2d572b77b7c7ad10e9e3d3b4f5
xnau <xnau@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-07-24 07:37:37 +0000

    major refactoring in how the cache is handled
    git-svn-id: http://plugins.svn.wordpress.org/participants-database/trunk@1459694 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/PDb_Live_Notification.class.php
+++ classes/PDb_Live_Notification.class.php
@@ -66,48 +66,65 @@ class PDb_Live_Notification {
    * gets the response body from a transient unless it has expired in which case 
    * it gets it from xnau.com
    * 
-   * @return string the response body
+   * @return object the response body
    */
   public function get_response_body()
   {
-    $response = Participants_Db::apply_filters( 'live_notification_cache_enable', true ) ? get_transient( $this->transient_name() ) : $this->get_response();
-    if ( ! $response ) {
-      $this->refresh_cached_response();
-      $response = get_transient( $this->transient_name() );
-    }
-    return json_decode( $response );
-  }
-
-  /**
-   * refreshes the response cache
-   * 
-   * @return bool true if the cache was stale and needed to be refreshed
-   */
-  private function refresh_cached_response()
-  {
-    $cache_is_stale = false;
-    $response = get_transient( $this->transient_name() );
-    if ( !$response ) {
-      $this->store_response( $this->get_response() );
-      $cache_is_stale = true;
-    }
-    error_log(__METHOD__.' getting response from ' . ($cache_is_stale ? 'xnau.com' : 'cache: ' . $this->transient_name() ) );
-    return $cache_is_stale;
+    return json_decode( $this->get_response() );
   }
 
   /**
    * gets the response
    * 
-   * @return string json-encoded response; empty string if the endpoint doesn't exist
+   * @return string json-encoded response
    */
   private function get_response()
   {
-    $response = '';
-    if ( $this->named_endpoint() ) {
+    $cached = Participants_Db::apply_filters( 'live_notification_cache_enable', true ) ? get_transient( $this->transient_name() ) : false;
+    if ( $cached === false && $this->remote_content_is_available() ) {
+      //error_log(__METHOD__.' getting remote content');
       $response = wp_remote_retrieve_body( wp_remote_get( $this->endpoint() ) );
-      error_log(__METHOD__.' response: '. $response );
+      $this->cache_response($response);
+    } elseif ( $cached !== false ) {
+      //error_log(__METHOD__.' getting cached content');
+      $response = $cached;
+    } else {
+      //error_log(__METHOD__.' providing blank content');
+      $response = json_encode( $this->blank_response() );
     }
     return $response;
+  }
+  
+  /**
+   * checks the remote content for availabilty
+   * 
+   * also checks that the endpoint is a valid request for a page
+   * 
+   * @return bool true if the content is available
+   */
+  private function remote_content_is_available()
+  {
+    $response = wp_remote_head( $this->endpoint() );
+    if ( is_wp_error($response) || empty( $this->named_endpoint() ) ) {
+      return false;
+    }
+    /*
+     * code should be 2xx or 3xx for a valid content response
+     */
+    //error_log(__METHOD__.' response code: '.$response['response']['code']);
+    return preg_match( '/^[23]\d{2}$/', $response['response']['code'] ) === 1;
+  }
+  
+  /**
+   * caches the supplied response string
+   * 
+   * @param string $response
+   */
+  private function cache_response( $response )
+  {
+    if ( Participants_Db::apply_filters( 'live_notification_cache_enable', true ) ) {
+      $this->store_response($response);
+    }
   }
 
   /**
@@ -117,9 +134,8 @@ class PDb_Live_Notification {
    */
   private function store_response( $response )
   {
-    if ( !empty( $response ) ) {
-      set_transient( $this->transient_name(), $response, PDb_Live_Notification_Handler::cache_lifetime() );
-    }
+    set_transient( $this->transient_name(), $response, PDb_Live_Notification_Handler::cache_lifetime() );
+    //error_log(__METHOD__.' storing: '.$this->transient_name().' expiration: '.PDb_Live_Notification_Handler::cache_lifetime());
   }
 
   /**
@@ -129,7 +145,7 @@ class PDb_Live_Notification {
    */
   private function transient_name()
   {
-    return self::cache_name . $this->name;
+    return Participants_Db::$prefix . self::cache_name . $this->name;
   }
 
   /**
@@ -152,6 +168,16 @@ class PDb_Live_Notification {
   private function named_endpoint()
   {
     return PDb_Live_Notification_Handler::content_id( $this->name );
+  }
+  
+  /**
+   * provides a blank response
+   * 
+   * @return object
+   */
+  private function blank_response()
+  {
+    return (object) array( 'content' => array( 'rendered' => '' ), 'title'  => array( 'rendered' => '' ) );
   }
 
 }
