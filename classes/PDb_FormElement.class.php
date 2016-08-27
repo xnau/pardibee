43db3e9d27800c65d4644dbf9bba7bfd864c3e5b
roland <webdesign@xnau.com>
2016-08-26 19:34:25 -1000

    added no validate filter to file delete checkbox

--- classes/PDb_FormElement.class.php
+++ classes/PDb_FormElement.class.php
@@ -530,7 +530,7 @@ class PDb_FormElement extends xnau_FormElement {
 
       // add the delete checkbox if there is a file defined
       if ( !empty( $this->value ) )
-        $this->_addline( '<span class="file-delete" ><label><input type="checkbox" value="delete" name="' . $this->name . '-deletefile" ' . $this->_attributes() . '>' . __( 'delete', 'participants-database' ) . '</label></span>' );
+        $this->_addline( '<span class="file-delete" ><label><input type="checkbox" value="delete" name="' . $this->name . '-deletefile" ' . $this->_attributes('no validate') . '>' . __( 'delete', 'participants-database' ) . '</label></span>' );
     }
 
     $this->_addline( '</div>' );
