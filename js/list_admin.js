1c748fa61095f8cf884b6811c4c8a33c834dff3a
roland <webdesign@xnau.com>
2017-01-15 22:13:19 -1000

    add over limit notice to pop-up confirm dialog

--- js/list_admin.js
+++ js/list_admin.js
@@ -2,7 +2,7 @@
  * js for handling general list management functions
  * 
  * @author Roland Barker, xnau webdesign
- * @version 0.4
+ * @version 0.5
  */
 var PDbListAdmin = (function ($) {
   "use strict";
@@ -68,7 +68,7 @@ var PDbListAdmin = (function ($) {
         },
         click : function () {
           checkState = true;
-          $(this).dialog("close");
+          $(this).dialog("close").html('');
         }
       }]
   });
@@ -76,9 +76,12 @@ var PDbListAdmin = (function ($) {
     init : function () {
       apply_button.on('click', function (e) {
         e.preventDefault();
-        var sense = (parseInt(count_element.val(), 10) > 1) ? 'plural' : 'singular';
+        var send_count = parseInt(count_element.val(), 10);
+        var sense = (send_count > 1) ? 'plural' : 'singular';
         var action = $('[name=with_selected]').val();
-        confirmDialog.html($('<h3/>').text(list_adminL10n.apply_confirm[action][sense])).dialog('open');
+        var overlimit = send_count > parseInt( list_adminL10n.send_limit );
+        var limit_message = overlimit ? $('<h4 class="dashicons-before dashicons-warning"/>').append(list_adminL10n.apply_confirm.recipient_count_exceeds_limit.replace('{limit}',list_adminL10n.send_limit)) : '';
+        confirmDialog.append($('<h3/>').append(list_adminL10n.apply_confirm[action][sense])).append(limit_message).dialog('open').find('a').blur();
       });
       checkall.click(checkAll);
       $('.delete-check').on('click', function () {
@@ -91,6 +94,6 @@ var PDbListAdmin = (function ($) {
   };
 }(jQuery));
 jQuery(function () {
-  "use sctrict";
+  "use strict";
   PDbListAdmin.init();
 });
