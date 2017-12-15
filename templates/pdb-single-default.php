dfb9256d5a5f4305dee6c682bf62ac94261eda78
roland <webdesign@xnau.com>
2017-12-15 08:26:03 -1000

    single template handles "not found" display #1639

--- templates/pdb-single-default.php
+++ templates/pdb-single-default.php
@@ -1,25 +1,12 @@
 <?php
-/*
- * default template for displaying a single record
+/**
+ * @name pdb single template default
+ * @version 2.0
  * 
- * this is the new "WordPress style" template
- *
- * each group with the "visible" attribute checked will display its fields in the order set 
- * in the manage database fields page.
- *
- * if there are specific fields you wish to exclude from display, you can include the "name" value of 
- * the field in the $exclude array like this: $exclude = array( 'city','state','country' ); or whatever 
- * you want. Leave it empty (like it is here) if you don't want to exclude any fields.
- *
- * this template is a simple demonstration of what is possible
- *
- * for those unfamiliar with PHP, just remember that something like <?php echo $group->name ?> just prints out 
- * the group name. You can move it around, but leave all the parts between the <> brackets as they are.
+ * default template for displaying a single record
  *
  */
-
-// define an array of fields to exclude here
-$exclude = array();
+if ( $this->participant_id > 0 ) :
 ?>
 
 <div class="wrap <?php echo $this->wrap_class ?>">
@@ -34,9 +21,6 @@ $exclude = array();
     
     
       <?php while ( $this->have_fields() ) : $this->the_field();
-      
-          // skip any field found in the exclude array
-          if ( in_array( $this->field->name, $exclude ) ) continue;
 					
           // CSS class for empty fields
 					$empty_class = $this->get_empty_class( $this->field );
@@ -57,3 +41,14 @@ $exclude = array();
   <?php endwhile; // end of the groups loop ?>
   
 </div>
+<?php else : // content to show if no record is found ?>
+
+  <?php $error_message = Participants_Db::plugin_setting( 'no_record_error_message', '' );
+  
+  if ( ! empty( $error_message ) ) : ?>
+
+    <p class="alert alert-error"><?php echo $error_message ?></p>
+    
+  <?php endif ?>
+    
+<?php endif ?>
