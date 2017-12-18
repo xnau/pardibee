ce4b42200b131a61e5b50ff418eb7695eac1bca9
roland <webdesign@xnau.com>
2017-12-17 15:43:26 -1000

    total refactor: messages are saved as message objects to an option several static API methods added

--- classes/PDb_Admin_Notices.class.php
+++ classes/PDb_Admin_Notices.class.php
@@ -1,20 +1,15 @@
 <?php
-
 /**
  * handles feedback messages in the admin
  * 
- * we can enable PHP errors in the admin messaging with this:
- *  set_error_handler( array( 'PDb_Admin_Notices', 'error_handler' ) );
- * 
- * remember to disconnect the handler when the script is finished:
- *  restore_error_handler();
+ * see the API calls for usage
  *
  * @package    WordPress
  * @subpackage Participants Database Plugin
- * @author     Alexandros Georgiou & Roland Barker <webdesign@xnau.com>
+ * @author     Roland Barker <webdesign@xnau.com>
  * @copyright  2017  xnau webdesign
  * @license    GPL3
- * @version    0.1
+ * @version    1.0
  * @link       https://www.alexgeorgiou.gr/persistently-dismissible-notices-wordpress/
  * @depends    
  */
@@ -27,23 +22,23 @@ class PDb_Admin_Notices {
    * @var PDb_Admin_Notices the class instance 
    */
   private static $_instance;
-  
+
   /**
    *
    * @var object  the current notices
    */
-  private $admin_notices;
+  private $admin_notice_list;
 
   /**
    * @var string list of message types
    */
   const TYPES = 'error,warning,info,success';
-  
+
   /**
-   * @var string  the option stem
+   * @var string  the option name
    */
-  const dismiss_option = 'pdb_admin_notices_dismissed_';
-  
+  const pdb_admin_notice = 'pdb_admin_notices';
+
   /**
    * @var string  GET var key
    */
@@ -52,28 +47,68 @@ class PDb_Admin_Notices {
   /**
    * these are our API calls
    * 
-   * @param string  $message
-   * @param bool    $dismiss_option
-   * @param bool    $force_show if true, overrides the dismiss
+   * the static calls are the most likely to be used, dynamic calls are for situations 
+   * where the object needs to be set up and modified before posting the message.
+   * 
+   * @param string  $message can inclue HTML, remember it is wrapped in a <p> tag.
+   * @param string  $context a context message (goes in the message heading)
+   * @param bool    $persistent if true, the message persists across page loads until dismissed
+   * 
+   * @return string unique id for the notice
    */
-  public function error( $message, $dismiss_option = true, $force_show = false )
+  public static function post_error( $message, $context = '', $persistent = true )
   {
-    $this->notice( 'error', $message, $dismiss_option, $force_show );
+    $notice = self::get_instance();
+    return $notice->error( $message, $context, $persistent );
   }
 
-  public function warning( $message, $dismiss_option = true, $force_show = false )
+  public function error( $message, $context = '', $persistent = true )
   {
-    $this->notice( 'warning', $message, $dismiss_option, $force_show );
+    return $this->notice( 'error', $message, $context, $persistent );
   }
 
-  public function success( $message, $dismiss_option = true, $force_show = false )
+  public static function post_warning( $message, $context = '', $persistent = true )
   {
-    $this->notice( 'success', $message, $dismiss_option, $force_show );
+    $notice = self::get_instance();
+    return $notice->warning( $message, $context, $persistent );
   }
 
-  public function info( $message, $dismiss_option = true, $force_show = false )
+  public function warning( $message, $context = '', $persistent = true )
   {
-    $this->notice( 'info', $message, $dismiss_option, $force_show );
+    return $this->notice( 'warning', $message, $context, $persistent );
+  }
+
+  public static function post_success( $message, $context = '', $persistent = true )
+  {
+    $notice = self::get_instance();
+    return $notice->success( $message, $context, $persistent );
+  }
+
+  public function success( $message, $context = '', $persistent = true )
+  {
+    return $this->notice( 'success', $message, $context, $persistent );
+  }
+
+  public static function post_info( $message, $context = '', $persistent = true )
+  {
+    $notice = self::get_instance();
+    return $notice->info( $message, $context, $persistent );
+  }
+
+  public function info( $message, $context = '', $persistent = true )
+  {
+    return $this->notice( 'info', $message, $context, $persistent );
+  }
+  
+  /**
+   * deletes the named notice
+   * 
+   * @param string  $id the notice id
+   */
+  public static function delete_notice( $id )
+  {
+    $notice = self::get_instance();
+    $notice->delete($id);
   }
 
   /**
@@ -81,14 +116,13 @@ class PDb_Admin_Notices {
    */
   private function __construct()
   {
-    $this->admin_notices = new stdClass();
-    foreach ( explode( ',', self::TYPES ) as $type ) {
-      $this->admin_notices->{$type} = array();
-    }
-    add_action( 'admin_init', array( $this, 'action_admin_init'), 20 );
-    add_action( 'admin_notices', array( $this, 'action_admin_notices') );
-    add_action( 'admin_enqueue_scripts', array( $this, 'action_admin_enqueue_scripts') );
-    add_action( 'participants_database_uninstall', array( $this, 'uninstall' ) );
+    $this->admin_notice_list = get_option( self::pdb_admin_notice, array() );
+    $this->purge_transient_notices();
+
+    add_action( 'admin_init', array($this, 'action_admin_init'), 20 );
+    add_action( 'admin_notices', array($this, 'action_admin_notices') );
+    add_action( 'admin_enqueue_scripts', array($this, 'action_admin_enqueue_scripts') );
+    add_action( 'participants_database_uninstall', array($this, 'uninstall') );
   }
 
   /**
@@ -111,7 +145,7 @@ class PDb_Admin_Notices {
   {
     $message_id = filter_input( INPUT_GET, self::get_key, FILTER_SANITIZE_STRING, FILTER_NULL_ON_FAILURE );
     if ( $message_id ) {
-      update_option( self::dismiss_option . $message_id, true );
+      $this->delete( $message_id );
       wp_die();
     }
   }
@@ -119,7 +153,7 @@ class PDb_Admin_Notices {
   public function action_admin_enqueue_scripts()
   {
     if ( $this->is_plugin_screen() ) {
-      wp_enqueue_script( Participants_Db::$prefix . 'admin-notices');
+      wp_enqueue_script( Participants_Db::$prefix . 'admin-notices' );
     }
   }
 
@@ -129,32 +163,57 @@ class PDb_Admin_Notices {
   public function action_admin_notices()
   {
     if ( $this->is_plugin_screen() ):
-      foreach ( explode( ',', self::TYPES ) as $type ) {
-      foreach ( $this->admin_notices->{$type} as $admin_notice ) {
+      
+      foreach ( $this->admin_notice_list as $admin_notice ) {
 
         $dismiss_url = add_query_arg( array(
             self::get_key => $admin_notice->id
                 ), admin_url() );
+        ?><div
+          class="notice pdb_admin_notices-notice notice-<?php
+          echo $admin_notice->type;
 
-        if ( !get_option( self::dismiss_option . $admin_notice->id ) ) {
-          ?><div
-            class="notice pdb_admin_notices-notice notice-<?php
-            echo $type;
+          echo ' is-dismissible" data-dismiss-url="' . esc_url( $dismiss_url );
+          ?>">
+          <h4><?php echo Participants_Db::$plugin_title . $admin_notice->context ?>:</h4>
+          <p><span class="dashicons <?php echo $this->dashicon( $admin_notice->type ) ?>"></span>&nbsp;<?php echo $admin_notice->message ?></p>
 
-            if ( $admin_notice->dismiss_option ) {
-              echo ' is-dismissible" data-dismiss-url="' . esc_url( $dismiss_url );
-            }
-            ?>">
-            <h4><?php echo Participants_Db::$plugin_title ?>:</h4>
-            <p><span class="dashicons <?php echo $this->dashicon( $type ) ?>"></span>&nbsp;<?php echo esc_html( $admin_notice->message ) ?></p>
-
-          </div><?php
-        }
+        </div><?php
       }
-    }
     endif;
   }
-  
+
+  /**
+   * removes a notice from the notices option
+   * 
+   * @param string  $notice_id
+   */
+  public function delete( $notice_id )
+  {
+    if ( isset( $this->admin_notice_list[$notice_id] ) ) {
+      unset( $this->admin_notice_list[$notice_id] );
+      $this->update_notices();
+    }
+  }
+
+  /**
+   * updates the notices option
+   */
+  private function update_notices()
+  {
+    update_option( self::pdb_admin_notice, $this->admin_notice_list );
+  }
+
+  /**
+   * purges all transient notices
+   */
+  private function purge_transient_notices()
+  {
+    $this->admin_notice_list = array_filter( $this->admin_notice_list, function ($notice) {
+      return $notice->persistent;
+    } );
+  }
+
   /**
    * tells if the current admin screen is a plugin screen
    * 
@@ -166,7 +225,7 @@ class PDb_Admin_Notices {
     $page = get_current_screen();
     return stripos( $page->id, Participants_Db::PLUGIN_NAME ) !== false;
   }
-  
+
   /**
    * provides the dashicons icon text
    * 
@@ -175,7 +234,7 @@ class PDb_Admin_Notices {
    */
   private function dashicon( $type )
   {
-    switch ($type) {
+    switch ( $type ) {
       case 'warning':
       case 'error':
         return 'dashicons-warning';
@@ -192,43 +251,16 @@ class PDb_Admin_Notices {
    * 
    * @param string  $type
    * @param string  $message
-   * @param bool    $dismiss_option if true, is dismissable
-   * @param bool    $force_show if true, overrides the dismiss
+   * @param string  $context a context string fro the message header
+   * @param bool    $persistent if true, message will persis across page loads
    */
-  private function notice( $type, $message, $dismiss_option, $force_show )
+  private function notice( $type, $message, $context, $persistent )
   {
-    $notice = new stdClass();
-    $notice->message = $message;
-    $notice->dismiss_option = $dismiss_option;
-    $notice->id = $this->notice_id( $message );
-    
-    if ( $force_show ) {
-      $this->clear_dismissed( $notice->id );
-    }
-    
-    $this->admin_notices->{$type}[] = $notice;
-  }
-  
-  /**
-   * provides a unique ID for each message
-   * 
-   * @param string $message the message
-   * @return string
-   */
-  private function notice_id( $message )
-  {
-    $current_user = wp_get_current_user();
-    return $current_user->ID . '-' . hash( 'crc32', $message );
-  }
-  
-  /**
-   * cleasrs a dismissed flag
-   * 
-   * @param string  $id the message id
-   */
-  private function clear_dismissed( $id )
-  {
-    delete_option( self::dismiss_option . $id );
+    $notice = new pdb_admin_notice_message($type,$message,$context,$persistent);
+
+    $this->admin_notice_list[$notice->id] = $notice;
+    $this->update_notices();
+    return $notice->id;
   }
 
   /**
@@ -273,7 +305,7 @@ class PDb_Admin_Notices {
     // Don't execute PHP internal error handler
     return true;
   }
-  
+
   /**
    * clears the options on uninstall
    * 
@@ -282,7 +314,103 @@ class PDb_Admin_Notices {
   public function uninstall()
   {
     global $wpdb;
-    $wpdb->query( 'DELETE FROM ' . $wpdb->prefix . 'options WHERE option_name LIKE "' . self::dismiss_option . '%";' );
+    $wpdb->query( 'DELETE FROM ' . $wpdb->prefix . 'options WHERE option_name LIKE "' . self::pdb_admin_notice . '%";' );
   }
 
+}
+
+/**
+ * models a single admin notice
+ */
+class pdb_admin_notice_message {
+  /**
+   * @var string the type of message
+   */
+  private $type;
+  
+  /**
+   * @var string the messgae text
+   */
+  private $message;
+  
+  /**
+   * @var string the context string
+   */
+  private $context;
+  
+  /**
+   * @var bool whether the message should be persistent across page loads
+   */
+  private $persistent;
+  
+  /**
+   * @var string holds the unique id for the message
+   */
+  private $id;
+  
+  /**
+   * @var string provides a joining string for using the context string in the message heading
+   */
+  private $context_joiner = '/';
+  
+  /**
+   * instantiates the message
+   * 
+   * 
+   * @param string  $type
+   * @param string  $message
+   * @param string  $context a context string fro the message header
+   * @param bool    $persistent if true, message will persist across page loads
+   */
+  public function __construct( $type, $message, $context, $persistent)
+  {
+    $this->type = $type;
+    $this->message = $message;
+    $this->context = $context;
+    $this->persistent = $persistent;
+    $this->notice_id( $message );
+  }
+  
+  /**
+   * provides object property values
+   * 
+   * @param string $name property name
+   * @return mixed
+   */
+  public function __get( $name )
+  {
+    switch ( $name ) {
+      case 'id':
+        return $this->id;
+      case 'message':
+        return wp_kses_post( $this->message );
+      case 'context':
+        return esc_html( $this->context_string() );
+      case 'persistent':
+        return (bool) $this->persistent;
+      case 'type':
+        return $this->type;
+    } 
+  }
+  
+  /**
+   * provides a context string
+   * 
+   * @return string
+   */
+  private function context_string()
+  {
+    return empty( $this->context ) ? '' : $this->context_joiner . $this->context;
+  }
+  /**
+   * provides a unique ID for each message
+   * 
+   * @param string $message the message
+   * @return string
+   */
+  private function notice_id( $message )
+  {
+    $current_user = wp_get_current_user();
+    $this->id = $current_user->ID . '-' . hash( 'crc32', $message );
+  }
 }
