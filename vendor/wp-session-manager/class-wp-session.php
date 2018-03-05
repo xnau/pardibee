b022f796e47cf59fd7bf44dc735e8291641fb14d
roland <webdesign@xnau.com>
2018-03-04 16:03:38 -1000

    update session manager to 2.02

--- vendor/wp-session-manager/class-wp-session.php
+++ vendor/wp-session-manager/class-wp-session.php
@@ -54,6 +54,15 @@ final class WP_Session extends Recursive_ArrayAccess {
 	 */
 	public static function get_instance() {
 		if ( ! self::$instance ) {
+            /**
+             * Initialize the session object and wire up any storage.
+             *
+             * Some operations (like database migration) need to be performed
+             * before the session is able to actually be populated with data.
+             * Ensure these operations are finished by wiring them to the
+             * session object's initialization hool.
+             */
+		    do_action('wp_session_init');
 			self::$instance = new self();
 		}
 
@@ -65,34 +74,39 @@ final class WP_Session extends Recursive_ArrayAccess {
 	 * Will rebuild the session collection from the given session ID if it exists. Otherwise, will
 	 * create a new session with that ID.
 	 *
-	 * @param $session_id
 	 * @uses apply_filters Calls `wp_session_expiration` to determine how long until sessions expire.
 	 */
-	protected function __construct() {
-		if ( isset( $_COOKIE[WP_SESSION_COOKIE] ) ) {
-			$cookie = stripslashes( $_COOKIE[WP_SESSION_COOKIE] );
-			$cookie_crumbs = explode( '||', $cookie );
+    protected function __construct() {
+        parent::__construct();
 
-            $this->session_id = preg_replace("/[^A-Za-z0-9_]/", '', $cookie_crumbs[0] );
-            $this->expires = absint( $cookie_crumbs[1] );
-            $this->exp_variant = absint( $cookie_crumbs[2] );
+        if (isset($_COOKIE[ WP_SESSION_COOKIE ])) {
+            $cookie = stripslashes($_COOKIE[ WP_SESSION_COOKIE ]);
+            $cookie_crumbs = explode('||', $cookie);
 
-			// Update the session expiration if we're past the variant time
-			if ( time() > $this->exp_variant ) {
-				$this->set_expiration();
-				delete_option( "_wp_session_expires_{$this->session_id}" );
-				add_option( "_wp_session_expires_{$this->session_id}", $this->expires, '', 'no' );
-			}
-		} else {
-			$this->session_id = WP_Session_Utils::generate_id();
-			$this->set_expiration();
-		}
+            $this->session_id = preg_replace("/[^A-Za-z0-9_]/", '', $cookie_crumbs[ 0 ]);
+            $this->expires = absint($cookie_crumbs[ 1 ]);
+            $this->exp_variant = absint($cookie_crumbs[ 2 ]);
 
-		$this->read_data();
+            // Update the session expiration if we're past the variant time
+            if (time() > $this->exp_variant) {
+                $this->set_expiration();
 
-		$this->set_cookie();
+                if (defined('WP_SESSION_USE_OPTIONS') && WP_SESSION_USE_OPTIONS) {
+                    update_option("_wp_session_expires_{$this->session_id}", $this->expires, 'no');
+                } else {
+                    WP_Session_Utils::update_session($this->session_id, array('session_expiry' => $this->expires));
+                }
+            }
+        } else {
+            $this->session_id = WP_Session_Utils::generate_id();
+            $this->set_expiration();
+        }
 
-	}
+        $this->read_data();
+
+        $this->set_cookie();
+
+    }
 
 	/**
 	 * Set both the expiration time and the expiration variant.
@@ -136,7 +150,11 @@ final class WP_Session extends Recursive_ArrayAccess {
 	 * @return array
 	 */
 	protected function read_data() {
-		$this->container = get_option( "_wp_session_{$this->session_id}", array() );
+        if (defined('WP_SESSION_USE_OPTIONS') && WP_SESSION_USE_OPTIONS) {
+            $this->container = get_option( "_wp_session_{$this->session_id}", array() );
+        } else {
+            $this->container = WP_Session_Utils::get_session( $this->session_id, array() );
+        }
 
 		return $this->container;
 	}
@@ -145,15 +163,39 @@ final class WP_Session extends Recursive_ArrayAccess {
 	 * Write the data from the current session to the data storage system.
 	 */
 	public function write_data() {
-		$option_key = "_wp_session_{$this->session_id}";
-		
-		if ( false === get_option( $option_key ) ) {
-			add_option( "_wp_session_{$this->session_id}", $this->container, '', 'no' );
-			add_option( "_wp_session_expires_{$this->session_id}", $this->expires, '', 'no' );
-		} else {
-			delete_option( "_wp_session_{$this->session_id}" );
-			add_option( "_wp_session_{$this->session_id}", $this->container, '', 'no' );
+	    // Nothing has changed, don't update the session
+	    if (!$this->dirty) {
+	        return;
+        }
+
+		// Session is dirty, but also empty. Purge it!
+		if( empty($this->container) ){
+            if (defined('WP_SESSION_USE_OPTIONS') && WP_SESSION_USE_OPTIONS) {
+                delete_option( "_wp_session_{$this->session_id}" );
+            } else {
+                WP_Session_Utils::delete_session( $this->session_id );
+            }
+
+			return;
 		}
+
+		// Session is dirty and needs to be updated, do so!
+        if (defined('WP_SESSION_USE_OPTIONS') && WP_SESSION_USE_OPTIONS) {
+            $option_key = "_wp_session_{$this->session_id}";
+
+            if ( false === get_option( $option_key ) ) {
+                add_option("_wp_session_{$this->session_id}", $this->container, '', 'no');
+                add_option("_wp_session_expires_{$this->session_id}", $this->expires, '', 'no');
+            } else {
+                update_option( "_wp_session_{$this->session_id}", $this->container, 'no' );
+            }
+        } else {
+		    if ( false === WP_Session_Utils::session_exists( $this->session_id ) ) {
+                WP_Session_Utils::add_session( array( 'session_key' => $this->session_id, 'session_value' => serialize($this->container), 'session_expiry' => $this->expires ) );
+            } else {
+                WP_Session_Utils::update_session( $this->session_id, array( 'session_value' => serialize($this->container) ) );
+            }
+        }
 	}
 
 	/**
@@ -173,7 +215,7 @@ final class WP_Session extends Recursive_ArrayAccess {
 	 * @return bool
 	 */
 	public function json_in( $data ) {
-		$array = json_decode( $data );
+		$array = json_decode( $data, true );
 
 		if ( is_array( $array ) ) {
 			$this->container = $array;
@@ -190,7 +232,11 @@ final class WP_Session extends Recursive_ArrayAccess {
 	 */
 	public function regenerate_id( $delete_old = false ) {
 		if ( $delete_old ) {
-			delete_option( "_wp_session_{$this->session_id}" );
+            if (defined('WP_SESSION_USE_OPTIONS') && WP_SESSION_USE_OPTIONS) {
+                delete_option( "_wp_session_{$this->session_id}" );
+            } else {
+                WP_Session_Utils::delete_session( $this->session_id );
+            }
 		}
 
 		$this->session_id = WP_Session_Utils::generate_id();
