f19d71d75f6b62175c92bbc86018eb0a58a3b483
roland <webdesign@xnau.com>
2018-05-18 10:16:40 -1000

    added brackets to if structure

--- classes/PDb_List_Admin.class.php
+++ classes/PDb_List_Admin.class.php
@@ -203,9 +203,10 @@ class PDb_List_Admin {
 
     self::_process_search();
 
-    if ( PDB_DEBUG )
+    if ( PDB_DEBUG ) {
       Participants_Db::debug_log( __METHOD__ . '
-list query: ' . self::list_query() );
+  list query: ' . self::list_query() );
+    }
     /*
      * save the query in a transient so it can be used by the export CSV functionality
      */
