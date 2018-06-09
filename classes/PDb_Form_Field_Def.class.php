a8bf064014084e8a1365992eff14905c5e101ce6
roland <webdesign@xnau.com>
2018-06-08 17:54:22 -1000

    make_readonly method can now also make a field not readonly by including false in the argument new method option_values for providing a list of just the defined option values with no titles

--- classes/PDb_Form_Field_Def.class.php
+++ classes/PDb_Form_Field_Def.class.php
@@ -8,96 +8,97 @@
  * @author     Roland Barker <webdesign@xnau.com>
  * @copyright  2018  xnau webdesign
  * @license    GPL3
- * @version    0.2
+ * @version    0.3
  * @link       http://xnau.com/wordpress-plugins/
  * @depends    
  */
-
 class PDb_Form_Field_Def {
+
   /**
    * @var int id of the field definition
    */
   private $id;
+
   /**
    * @var int order value of the field definition
    */
   private $order;
-  
+
   /**
    * @var string name of the field
    */
   private $name;
-  
+
   /**
    * @var string display title of the field
    */
   private $title;
-  
+
   /**
    * @var string default value of the field
    */
   public $default;
-  
+
   /**
    * @var string name of the field's group
    */
   private $group;
-  
+
   /**
    * @var string field help text
    */
   public $help_text;
-  
+
   /**
    * @var string form element of the field
    */
   private $form_element;
-  
+
   /**
    * @var string raw string form the "values" parameter of the field def
    */
   private $values;
-  
+
   /**
    * @var string validation type
    */
   private $validation;
-  
+
   /**
    * @var int display column value
    */
   private $display_column;
-  
+
   /**
    * @var int admin column value
    */
   private $admin_column;
-  
+
   /**
    * @var bool field is sortable
    */
   private $sortable;
-  
+
   /**
    * @var bool field is included in CSV export
    */
   private $CSV;
-  
+
   /**
    * @var bool field is persistent
    */
   private $persistent;
-  
+
   /**
    * @var bool field is included in the signup form
    */
   private $signup;
-  
+
   /**
    * @var bool field is read only
    */
   private $readonly;
-  
+
   /**
    * @var string name of the current module
    */
@@ -112,7 +113,7 @@ class PDb_Form_Field_Def {
    * @var array element options
    */
   public $options = array();
-  
+
   /**
    * instantiates the object
    * 
@@ -120,12 +121,12 @@ class PDb_Form_Field_Def {
    */
   public function __construct( $field )
   {
-    assert( is_string( $field ) || property_exists( $field, 'name' ), ' improperly instantiated with: ' . print_r($field,1) );
-    $def = is_string( $field ) ? self::get_field_def($field) : $field;
-    
+    assert( is_string( $field ) || property_exists( $field, 'name' ), ' improperly instantiated with: ' . print_r( $field, 1 ) );
+    $def = is_string( $field ) ? self::get_field_def( $field ) : $field;
+
     $this->assign_def_props( $def );
   }
-  
+
   /**
    * provides an instance of the object, given a field name
    * 
@@ -136,7 +137,7 @@ class PDb_Form_Field_Def {
   {
     return $result = self::get_field_def( $fieldname ) ? new self( $result ) : false;
   }
-  
+
   /**
    * tells if the field def is in the db
    * 
@@ -145,14 +146,14 @@ class PDb_Form_Field_Def {
    * @return bool
    */
   public static function is_field( $fieldname )
-  { 
+  {
     global $wpdb;
     $sql = 'SELECT COUNT(*) 
             FROM ' . Participants_Db::$fields_table . ' v 
             WHERE v.name = %s';
     return (bool) $wpdb->get_var( $wpdb->prepare( $sql, $fieldname ) );
   }
-  
+
   /**
    * supplies the field definition data from the db
    * 
@@ -161,14 +162,13 @@ class PDb_Form_Field_Def {
    * @return object
    */
   private static function get_field_def( $fieldname )
-  { 
+  {
     global $wpdb;
     $sql = 'SELECT v.* 
             FROM ' . Participants_Db::$fields_table . ' v 
             WHERE v.name = %s';
     return current( $wpdb->get_results( $wpdb->prepare( $sql, $fieldname ) ) );
   }
-
 
   /**
    * provides a property value
@@ -181,13 +181,13 @@ class PDb_Form_Field_Def {
 //    error_log(__METHOD__.' getting property: '.$prop.' 
 //      
 //trace: '.print_r(wp_debug_backtrace_summary(),1));
-    assert( property_exists($this, $prop ),' undefined property: ' . $prop );
+    assert( property_exists( $this, $prop ), ' undefined property: ' . $prop );
     switch ( $prop ) {
       default:
         return $this->{$prop};
     }
   }
-  
+
   /**
    * sets a property value
    * 
@@ -197,27 +197,33 @@ class PDb_Form_Field_Def {
    */
   public function __set( $prop, $value )
   {
-    switch ($prop) {
+    switch ( $prop ) {
       case 'value':
         $this->{$prop} = $value;
         break;
       default:
-        error_log(__METHOD__.' setting property: '.$prop.' 
+        error_log( __METHOD__ . ' setting property: ' . $prop . ' 
 
-    trace: '.print_r(wp_debug_backtrace_summary(),1));
+    trace: ' . print_r( wp_debug_backtrace_summary(), 1 ) );
         $this->{$prop} = $value;
     }
   }
-  
+
   /**
    * makes the field a read-only field
+   * 
+   * @param bool $readonly if true, makes the field a readonly field, if false, makes the field a public writable field
    */
-  public function make_readonly()
+  public function make_readonly( $readonly = true )
   {
-    $this->readonly = true;
-    $this->form_element = 'text-line';
+    if ( $readonly ) {
+      $this->readonly = true;
+      $this->form_element = 'text-line';
+    } else {
+      $this->readonly = false;
+    }
   }
-  
+
   /**
    * sets the readonly state
    * 
@@ -227,7 +233,7 @@ class PDb_Form_Field_Def {
   {
     $this->readonly = (bool) $state;
   }
-  
+
   /**
    * provides the name of the field
    * 
@@ -237,7 +243,7 @@ class PDb_Form_Field_Def {
   {
     return $this->name;
   }
-  
+
   /**
    * provides the default value for the field
    * 
@@ -247,7 +253,7 @@ class PDb_Form_Field_Def {
   {
     return $this->default;
   }
-  
+
   /**
    * tells the field's form element
    * 
@@ -257,7 +263,7 @@ class PDb_Form_Field_Def {
   {
     return $this->form_element;
   }
-  
+
   /**
    * tells the field group
    * 
@@ -267,7 +273,7 @@ class PDb_Form_Field_Def {
   {
     return $this->group;
   }
-  
+
   /**
    * provides a "value title" if defined
    * 
@@ -277,18 +283,28 @@ class PDb_Form_Field_Def {
   public function value_title( $value )
   {
     if ( $this->is_value_set() ) {
-        foreach ( $this->options as $option_title => $option_value ) {
-          if ( !is_string( $option_title ) || $option_title === 'other' ) {
-            // do nothing: we use the stored value
-          } elseif ( str_replace(' ','',$option_value) === $value ) { // strip out spaces in the option because we did that to the value
-            // grab the option title
-            return Participants_Db::apply_filters( 'translate_string', stripslashes( $option_title ) );
-          }
+      foreach ( $this->options as $option_title => $option_value ) {
+        if ( !is_string( $option_title ) || $option_title === 'other' ) {
+          // do nothing: we use the stored value
+        } elseif ( str_replace( ' ', '', $option_value ) === $value ) { // strip out spaces in the option because we did that to the value
+          // grab the option title
+          return Participants_Db::apply_filters( 'translate_string', stripslashes( $option_title ) );
         }
+      }
     }
     return $value;
   }
-  
+
+  /**
+   * tells if the field is readonly
+   * 
+   * @return bool
+   */
+  public function is_readonly()
+  {
+    return (bool) $this->readonly;
+  }
+
   /**
    * tells if the field is an internal field
    * 
@@ -298,7 +314,7 @@ class PDb_Form_Field_Def {
   {
     return $this->group === 'internal';
   }
-  
+
   /**
    * tells if the field is a hidden field
    * 
@@ -308,7 +324,7 @@ class PDb_Form_Field_Def {
   {
     return $this->form_element === 'hidden';
   }
-  
+
   /**
    * tells if the form element is a "value set" type element
    * 
@@ -318,7 +334,7 @@ class PDb_Form_Field_Def {
   {
     return PDb_FormElement::is_value_set( $this->form_element );
   }
-  
+
   /**
    * tells if the field is a dynamic hidden field
    * 
@@ -328,7 +344,7 @@ class PDb_Form_Field_Def {
   {
     return $this->is_hidden_field() && Participants_Db::is_dynamic_value( $this->value );
   }
-  
+
   /**
    * tells if the field is the single record link field
    * 
@@ -336,9 +352,19 @@ class PDb_Form_Field_Def {
    */
   public function is_single_record_link_field()
   {
-    return !in_array( $this->form_element, array('rich-text', 'link' ) ) && Participants_Db::is_single_record_link($this);
+    return !in_array( $this->form_element, array('rich-text', 'link') ) && Participants_Db::is_single_record_link( $this );
   }
   
+  /**
+   * tells if the field has the persistent flag set
+   * 
+   * @return bool
+   */
+  public function is_persistent()
+  {
+    return (bool) $this->persistent;
+  }
+
   /**
    * tells if the field has a default value
    * 
@@ -348,7 +374,7 @@ class PDb_Form_Field_Def {
   {
     return $this->default_value() !== '';
   }
-  
+
   /**
    * provides the values array
    * 
@@ -360,8 +386,31 @@ class PDb_Form_Field_Def {
   {
     return (array) maybe_unserialize( $this->values );
   }
-    
-  
+
+  /**
+   * provides the field options
+   * 
+   * this is really only valid with form elements that have options
+   * 
+   * @retrun array of options as $title => $option
+   */
+  public function options()
+  {
+    return $this->options;
+  }
+
+  /**
+   * provides an array of option values
+   * 
+   * this provides an indexed array of just the avlues, titles are ignored
+   * 
+   * @return array as $index => $value
+   */
+  public function option_values()
+  {
+    return PDb_FormElement::is_assoc( $this->options ) ? array_values( $this->options ) : $this->options;
+  }
+
   /**
    * assigns the object properties
    * 
@@ -371,7 +420,7 @@ class PDb_Form_Field_Def {
   {
     foreach ( $def as $prop => $value ) {
       switch ( $prop ) {
-        
+
         case 'sortable':
         case 'CSV':
         case 'persistent':
@@ -379,9 +428,9 @@ class PDb_Form_Field_Def {
         case 'readonly':
           $this->{$prop} = (bool) $value;
           break;
-        
+
         case 'values':
-          
+
           $this->values = $def->values;
           $values = $this->values_array();
           /*
@@ -401,4 +450,5 @@ class PDb_Form_Field_Def {
       }
     }
   }
+
 }
