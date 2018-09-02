b20acd23b0e4d78deba3edd121db848ef6b7df91
roland <webdesign@xnau.com>
2018-09-01 15:59:40 -1000

    spelling

--- classes/PDb_FormElement.class.php
+++ classes/PDb_FormElement.class.php
@@ -170,7 +170,7 @@ class PDb_FormElement extends xnau_FormElement {
    * 
    * this supplants the function Participants_Db::prep_field_for_display
    * 
-   * @param object|string $field a Field_Item object or field name
+   * @param object|string $field a PDb_Field_Item object or field name
    * @param bool   $html  if true, retuns the value wrapped in HTML, false returns 
    *                      the formatted value alone
    * @return string the object's current value, formatted
