af72fa0ff8374c3ea2a1800e699e2a8e8edddbf2
roland <webdesign@xnau.com>
2018-10-21 14:37:30 -1000

    added a template that just prints a bare value with no html

---
+++ templates/pdb-single-bare-value.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * template for showing a single value with no tags from a record
+ *
+ * the field to show must be defined in the shortcode using the "fields" attribute: 
+ * [pdb_single template="bare-value" fields="first_name"]
+ * 
+ * this template requres the id of the record to get the value from be provided, 
+ * either as a variable in the URI (?pdb=123) or as a shortcode attribute 
+ * [pdb_single template="bare-value" record_id="123" fields="first_name"]
+ *
+ */
+while ($this->have_groups()) : $this->the_group();
+ 
+  while ($this->have_fields()) : $this->the_field();
+  ?>
+ 		
+    <?php $this->field->print_value() ?>
+    
+  <?php
+    endwhile;
+    
+  break;
+ 	
+endwhile;
