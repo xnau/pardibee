3f12b5e499329aa14e7ad16ff95642102b89b54c
roland <webdesign@xnau.com>
2019-02-21 18:15:51 -1000

    fixed the csv preference detection #2005

--- classes/PDb_CSV_Import.class.php
+++ classes/PDb_CSV_Import.class.php
@@ -50,7 +50,8 @@ class PDb_CSV_Import extends xnau_CSV_Import {
 
     foreach ( $columns as $column ) {
     
-      if ( $column->CSV != '0') $this->column_names[] = $column->name;
+      if ( $column->CSV )
+        $this->column_names[] = $column->name;
     
     }
     
