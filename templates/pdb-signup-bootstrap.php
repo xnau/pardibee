8bd8ea8e1c13491f4b956f8d4f5e16ea4e86ce10
roland <webdesign@xnau.com>
2019-04-01 21:39:33 -1000

    fixed issue with fields not shown shown group labels are off #2046

--- templates/pdb-signup-bootstrap.php
+++ templates/pdb-signup-bootstrap.php
@@ -5,7 +5,7 @@
  * outputs a Twitter Bootstrap-compatible form
  * http://twitter.github.com/bootstrap/index.html
  * 
- * @version 0.4
+ * @version 0.5
  *
  */
 ?>
@@ -17,51 +17,53 @@
   $this->print_errors( '<div class="alert %1$s">%2$s</div>', '<p>%s</p>' );
   ?>
 
-<?php $this->print_form_head(); // this must be included before any fields are output   ?>
+  <?php $this->print_form_head(); // this must be included before any fields are output   ?>
 
   <div class="form-horizontal pdb-signup">
 
     <?php while ( $this->have_groups() ) : $this->the_group(); ?>
 
-  <?php if ( $this->group->has_fields() && $this->group->printing_title() ) : ?>
+      <?php if ( $this->group->has_fields() ) : ?>
 
-        <fieldset class="field-group field-group-<?php echo $this->group->name ?>">
-          <?php $this->group->print_title( '<legend>', '</legend>' ) ?>
-          <?php $this->group->print_description() ?>
+        <?php if ( $this->group->printing_title() ) : ?>
+          <fieldset class="field-group field-group-<?php echo $this->group->name ?>">
+            <?php $this->group->print_title( '<legend>', '</legend>' ) ?>
+            <?php $this->group->print_description() ?>
+          <?php endif ?>
 
           <?php while ( $this->have_fields() ) : $this->the_field(); ?>
 
-      <?php $feedback_class = $this->field->has_error() ? 'error' : ''; ?>
+            <?php $feedback_class = $this->field->has_error() ? 'error' : ''; ?>
 
             <div class="<?php $this->field->print_element_class() ?> control-group <?php echo $feedback_class ?>">
               <?php if ( $this->field->has_title() ) : ?>
-                <label class="control-label" for="<?php $this->field->print_element_id() ?>" ><?php $this->field->print_label(); // this function adds the required marker ?></label>
-      <?php endif ?>
+                <label class="control-label" for="<?php $this->field->print_element_id() ?>" ><?php $this->field->print_label(); // this function adds the required marker   ?></label>
+              <?php endif ?>
               <div class="controls"><?php $this->field->print_element_with_id(); ?>
 
-                  <?php if ( $this->field->has_help_text() ) : ?>
+                <?php if ( $this->field->has_help_text() ) : ?>
                   <span class="help-block">
-                  <?php $this->field->print_help_text() ?>
+                    <?php $this->field->print_help_text() ?>
                   </span>
-      <?php endif ?>
+                <?php endif ?>
 
               </div>
 
             </div>
 
-    <?php endwhile ?>
-
-        </fieldset>
-
+          <?php endwhile ?>
+          <?php if ( $this->group->printing_title() ) : ?>
+          </fieldset>
+          <?php endif ?>
       <?php endif ?>
 
-<?php endwhile; // groups   ?>
+    <?php endwhile; // groups   ?>
     <fieldset class="field-group field-group-submit">
       <div id="submit-button" class="controls">
         <?php $this->print_submit_button( 'btn btn-primary' ); // you can specify a class for the button ?>
-<?php $this->print_retrieve_link(); ?>
+        <?php $this->print_retrieve_link(); ?>
       </div>
     </fieldset>
   </div>
-<?php $this->print_form_close() ?>
+  <?php $this->print_form_close() ?>
 </div>
