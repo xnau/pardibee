b3c3bd0dcd8b947681cc7c6de7a86070ba5e805e
roland <webdesign@xnau.com>
2019-11-29 10:25:15 -1000

    email send now uses the raw text replace if html emails are disabled #2076

--- classes/PDb_Template_Email.php
+++ classes/PDb_Template_Email.php
@@ -82,8 +82,11 @@ class PDb_Template_Email extends xnau_Template_Email {
   protected function send_email()
   {
     $body = $this->html_format() ? PDb_Tag_Template::replaced_rich_text( $this->email_template, $this->data ) : PDb_Tag_Template::replaced_text_raw( $this->email_template, $this->data );
+    
     $success = $this->_mail( $this->email_to, PDb_Tag_Template::replaced_text_raw( $this->email_subject, $this->data ), $body );
+    
     remove_filter('pdb-tag_template_field_display_value', array( $this, 'clean_display_values' ) );
+    
     return $success;
   }
   
