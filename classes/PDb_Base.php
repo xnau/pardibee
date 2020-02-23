ea4337bb5f3bdb00012934af6f1c36c56ed6443e
roland <webdesign@xnau.com>
2020-02-23 09:10:48 -1000

    added function for handling finding the permalink for a post ID and filtering the result #2296

--- classes/PDb_Base.php
+++ classes/PDb_Base.php
@@ -475,13 +475,34 @@ class PDb_Base {
     } elseif ( $post = get_page_by_path( $page ) ) {
       $id = $post->ID;
     } else {
+      // get the ID by the post slug
       global $wpdb;
       $id = $wpdb->get_var( $wpdb->prepare( "SELECT p.ID FROM $wpdb->posts p WHERE p.post_name = '%s' AND p.post_status = 'publish'", trim( $page, '/ ' ) ) );
-//      error_log(__METHOD__.' query: '.$wpdb->last_query);
+      
     }
     if ( $id )
-      $permalink = get_permalink( $id );
+      $permalink = self::get_permalink( $id );
     return $permalink;
+  }
+  
+  /**
+   * provides the permalink for a WP page or post given the ID
+   * 
+   * this implements a filter to allow a multilingual plugin to alter the ID
+   * 
+   * @param int $id the post ID
+   * @return string the permalink
+   */
+  public static function get_permalink( $id )
+  {
+    /**
+     * allow a multilingual plugin to set the language post id
+     * 
+     * @filter pdb-lang_page_id
+     * @param int the post ID
+     * @return string the page or post permalink
+     */
+    return Participants_Db::apply_filters( 'lang_page_id', get_permalink( $id ) );
   }
 
   /**
