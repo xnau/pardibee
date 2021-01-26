0d787eeee9f1da4f45146e13aacaf30b0658e6a2
roland <webdesign@xnau.com>
2021-01-25 17:32:44 -1000

    refactored to use a static function for the actual tag replacement so that its functionality can be more easily used by other classes and plugins

--- classes/PDb_Tag_Template.php
+++ classes/PDb_Tag_Template.php
@@ -170,29 +170,42 @@ class PDb_Tag_Template {
      * @return array
      */
     $tag_data = Participants_Db::apply_filters( 'tag_template_data_before_replace', $this->data );
-
-    $placeholders = array();
-
-    for ( $i = 1; $i <= count( $tag_data ); $i++ ) {
-      $placeholders[] = '%' . $i . '$s';
-    }
-
-    // gets the tag strings and wrap them in brackets to match them in the template
-    $tags = array_map( function ( $v ) {
-      return '[' . $v . ']';
-    }, array_keys( $tag_data ) );
-
-    // replace the tags with variables
-    $pattern = str_replace( $tags, $placeholders, $this->template );
-
-    // replace the variables with strings
-    $fulltext = vsprintf( $pattern, $tag_data );
+    
+    $fulltext = self::replace_text( $this->template, $tag_data );
 
 //    error_log(__METHOD__.' pattern: '.$pattern.' data: '.print_r($this->data,1));
 //    error_log(__METHOD__.' rich text? '.($this->rich_text?'yes':'no'). '   '.$fulltext);
 //    error_log(__METHOD__.' template: '.$this->template);
 
     return $this->rich_text ? Participants_Db::process_rich_text( $fulltext, 'tag template' ) : $fulltext;
+  }
+  
+  /**
+   * replaces the tags in a template with data
+   * 
+   * @param string $template
+   * @param array $data as $name=>$value
+   * @return string replaced text
+   */
+  public static function replace_text( $template, $data )
+  {
+    $placeholders = array();
+
+    for ( $i = 1; $i <= count( $data ); $i++ ) {
+      $placeholders[] = '%' . $i . '$s';
+    }
+
+    // gets the tag strings and wrap them in brackets to match them in the template
+    $tags = array_map( function ( $v ) {
+      return '[' . $v . ']';
+    }, array_keys( $data ) );
+
+    // replace the tags with variables
+    $pattern = str_replace( $tags, $placeholders, $template );
+    
+
+    // replace the variables with strings
+    return vsprintf( $pattern, $data );
   }
 
   /**
