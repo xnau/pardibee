b81bd91cf19604d4c0a8aa0a9e8273ffca72cd47
roland <webdesign@xnau.com>
2021-03-10 14:01:34 -1000

    allow setting the attribute label when instantiating the PDb_Field_Def_Parameter object

--- classes/PDb_Field_Editor.php
+++ classes/PDb_Field_Editor.php
@@ -8,7 +8,7 @@
  * @author     Roland Barker <webdesign@xnau.com>
  * @copyright  2018  xnau webdesign
  * @license    GPL3
- * @version    0.3
+ * @version    0.4
  * @link       http://xnau.com/wordpress-plugins/
  * @depends    
  */
@@ -705,7 +705,7 @@ class PDb_Field_Def_Parameter {
   {
     $this->name = $name;
     $this->config = $config;
-    $this->label = $this->get_label();
+    $this->label = isset( $config['label'] ) && ! empty( $config['label'] ) ? $config['label'] : $this->get_label();
   }
 
   /**
