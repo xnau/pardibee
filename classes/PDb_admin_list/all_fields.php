e5ab1ba2c2509b30a5f0b1a2dbbfc74c8b0adff6
roland <webdesign@xnau.com>
2021-03-20 08:41:10 -1000

    new classes to handle defining search field groups in the admin list #2423

--- classes/PDb_admin_list/all_fields.php
+++ classes/PDb_admin_list/all_fields.php
@@ -1,23 +1,21 @@
 <?php
 
 /**
- * models a list of text fields for use in a multi-field search filter
- * 
+ * provides the fields for an "all fields" search
  *
  * @package    WordPress
  * @subpackage Participants Database Plugin
  * @author     Roland Barker <webdesign@xnau.com>
  * @copyright  2021  xnau webdesign
  * @license    GPL3
- * @version    0.2
+ * @version    0.1
  * @link       http://xnau.com/wordpress-plugins/
  * @depends    
  */
 
 namespace PDb_admin_list;
-use \Participants_Db;
 
-class text_fields extends search_field_group {
+class all_fields extends search_field_group {
   
   /**
    * tests the field for inclusion
@@ -27,18 +25,18 @@ class text_fields extends search_field_group {
    */
   protected function include_field( $field )
   {
-    return in_array( $field->form_element(), $this->valid_types() );
+    return $this->_include_field( $field );
   }
   
   /**
-   * provides a list of valid form elements to include
+   * tells if the field type should be included
    * 
-   * @return array
+   * @param \PDb_Form_Field_Def $field
+   * @return bool
    */
-  private function valid_types()
+  private function _include_field( $field )
   {
-    return Participants_Db::apply_filters('valid_types_for_admin_list_search_text_fields', array(
-        'text-line', 'text-area', 'rich-text'
-    ) );
+    return \Participants_Db::apply_filters( 'valid_type_for_admin_list_search_all_fields', $field->stores_data(), $field );
   }
+  
 }
