82e324972032c27ac03e07c95d74a4f060c06c95
roland <webdesign@xnau.com>
2021-03-22 18:59:08 -1000

    a space character is no longer a valid search term #2501

--- classes/PDb_List_Query.php
+++ classes/PDb_List_Query.php
@@ -568,7 +568,7 @@ class PDb_List_Query {
   private function search_term_is_valid( $term )
   {
     foreach ( (array) $term as $t ) {
-      $valid = strlen( trim( $t, '*?_%.' ) ) > 0 || ( Participants_Db::plugin_setting_is_true( 'empty_search' ) && $t === '' );
+      $valid = strlen( trim( urldecode( $t ), '*?_%. ' ) ) > 0 || ( Participants_Db::plugin_setting_is_true( 'empty_search' ) && $t === '' );
 
       if ( !$valid ) {
         break;
