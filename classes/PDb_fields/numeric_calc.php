8c3f538dc3238123cd496b0682de8005b261e390
roland <webdesign@xnau.com>
2021-10-15 08:40:58 -1000

    replaced_string method returns false if there is data missing from the template to prevent incorrect calculations from getting displayed #2498 now using the calculations trait

--- classes/PDb_fields/numeric_calc.php
+++ classes/PDb_fields/numeric_calc.php
@@ -15,7 +15,9 @@
 
 namespace PDb_fields;
 
-class numeric_calc extends templated_field {
+class numeric_calc extends calculated_field {
+  
+  use calculations;
 
   /**
    * @var string name of the form element
@@ -31,14 +33,6 @@ class numeric_calc extends templated_field {
    * @var array of calculation steps
    */
   private $calc_list;
-  
-  /**
-   * 
-   */
-  public function __construct()
-  {
-    parent::__construct( self::element_name );
-  }
   
   /**
    * provides the field's title
@@ -59,6 +53,10 @@ class numeric_calc extends templated_field {
   protected function replaced_string( $data )
   {
     $replacement_data = $this->replacement_data( $data );
+    
+    if ( ! $this->check_data( $replacement_data ) ) {
+      return false;
+    }
     
     return \PDb_Tag_Template::replace_text( $this->prepped_template(), $replacement_data );
   }
@@ -118,239 +116,11 @@ class numeric_calc extends templated_field {
       }
     }
     
-    $replacement_data[ self::calc_tag ] = $this->calculated_value( $replacement_data );
+    $this->calculate_value( $replacement_data );
+    
+    $replacement_data[ self::calc_tag ] = $this->result;
     
     return $replacement_data;
-  }
-  
-  /**
-   * provides the calculated total
-   * 
-   * @param array $replacement_data the prepared data from the template
-   * @return string
-   */
-  private function calculated_value( $replacement_data )
-  {
-    error_log(__METHOD__.' template: '.$this->calc_template() );
-    error_log(__METHOD__.' replacement data: '.print_r($replacement_data,1));
-    
-    $this->calc_list($replacement_data);
-    
-    error_log(__METHOD__.' calc list: '.print_r($this->calc_list,1));
-    
-    return $this->get_calculated_value();
-  }
-  
-  /**
-   * performs the calculation on the calc list
-   * 
-   * @return int|float
-   */
-  private function get_calculated_value()
-  {
-    // first pass to calc the sums
-    $sum_list = array();
-    $sum_count = 0;
-    $sum = 0;
-    $op = 'add';
-    
-    foreach( $this->calc_list as $i => $item ) {
-      
-      switch ( $item ) {
-        
-        case '+':
-          $op = 'add';
-          break;
-        
-        case '-':
-          $op = 'sub';
-          break;
-        
-        case '=':
-        case '/':
-        case '*':
-          $sum_list[] = $sum;
-          $sum = 0;
-          break;
-        
-        default:
-          if ( is_numeric( $item ) ) {
-            if ( $op === 'add' ) {
-              $sum = $sum + $item;
-              $sum_count++;
-            } elseif ( $op === 'sub' ) {
-              $sum = $sum - $item;
-            }
-          }
-          
-      }
-      
-    error_log(__METHOD__.' sum: '.$sum.' sum count: '.$sum_count.' sum list: '.print_r($sum_list,1));
-      
-    }
-    
-    $product = null;
-    $sum_index = 0;
-    // now perform the multiplication or division
-    foreach( $this->calc_list as $i => $item ) {
-      
-      switch ( $item ) {
-        
-        case '*':
-          $product = $sum_list[$sum_index] * $sum_list[$sum_index+1];
-          $sum_index = $sum_index+2;
-          break;
-        
-        case '/':
-          $product = $sum_list[$sum_index] / $sum_list[$sum_index+1];
-          $sum_index = $sum_index+2;
-          break;
-        
-        case '=':
-          if ( is_null( $product ) ) {
-            $product = $sum_list[$sum_index];
-            $sum_index++;
-          }
-          $format = $this->calc_list[$i+1];
-          break 2;
-      }
-    }
-    
-    $total = $product;
-    
-    if ( $format ) {
-      $total = $this->format( $total, $format, $sum_count );
-    }
-    
-    return $total;
-  }
-  
-  /**
-   * formats the value based on the formatting tag
-   * 
-   * @param int|float $value
-   * @param string $format_tag the format tag
-   * @param int $sum_count the number of summed items
-   * @return string the formatted value
-   */
-  private function format( $value, $format_tag, $sum_count )
-  {
-    // remove the leading ?
-    $format_tag = str_replace( '?', '', $format_tag );
-    
-    if ( preg_match( '/(.+_)(\d+)$/', $format_tag, $matches ) === 1 ) {
-      $numeric = $matches[2];
-      $format_tag = $matches[1] . 'n';
-    }
-    
-    switch ($format_tag) {
-      
-      case 'round_n':
-        $formatted = number_format( $value, $numeric );
-        break;
-      
-      case 'integer':
-        $formatted = $this->truncate_number( $value );
-        break;
-      
-      case 'year':
-      case 'week':
-      case 'month':
-      case 'day':
-        $formatted = $this->truncate_number( $value / constant( strtoupper( $format_tag ) . '_IN_SECONDS' ) );
-        break;
-      
-      case 'average':
-      case 'average_n':
-        $average = $sum_count > 0 ? $value / $sum_count : $value;
-        $formatted = number_format( $average, $numeric );
-        break;
-        
-      default:
-        $formatted = $value;
-    }
-    
-    return $formatted;
-  }
-  
-  /**
-   * truncates the fractional part of a float
-   * 
-   * @param float $number
-   * @return int
-   */
-  private function truncate_number( $number )
-  {
-    if ( strpos( $number, '.' ) === false ) {
-      return $number;
-    }
-    
-    list( $int, $frac ) = explode( '.', $number );
-    
-    return $int;
-  }
-  
-  /**
-   * provides the calc list array
-   * 
-   * @param array $data replacement data
-   * @return array
-   */
-  private function calc_list ( $data )
-  {
-    $calc_list = array();
-    $tag = false;
-    $buffer = '';
-    
-    foreach( str_split( $this->calc_template() ) as $chr ) {
-      
-      switch ($chr) {
-        
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-        case '=':
-          if ( is_numeric( $buffer ) ) {
-            // add any literal number
-            $calc_list[] = $buffer;
-          }
-          $calc_list[] = $chr;
-          $buffer = '';
-          break;
-        
-        case '[':
-          $tag = true;
-          $buffer = '';
-          break;
-        
-        case ']':
-          $tag = false;
-          
-          /* tag is closed: get the value from the data, but only if it is a 
-           * number of some kind
-           */
-          if ( isset( $data[$buffer] ) && is_numeric( $data[$buffer] ) ) {
-            
-            // get the value of the tag
-            $calc_list[] = $data[$buffer];
-            
-          } elseif ( preg_match( '/^(#|\?).+/', $buffer ) ) {
-            
-            // this is a key value, add it to the list as-is
-            $calc_list[] = $buffer;
-            
-          }
-          
-          $buffer = '';
-          break;
-          
-        default:
-          $buffer .= $chr;
-      }
-    }
-    
-    $this->calc_list = $calc_list;
   }
   
 }
