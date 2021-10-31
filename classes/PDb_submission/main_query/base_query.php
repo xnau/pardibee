2964b3eccff5a6ea5c88c38f717a5b4d24211492
roland <webdesign@xnau.com>
2021-10-31 11:00:55 -1000

    added is_csv_import public method to provide a single method for determining if the request is a CSV import

--- classes/PDb_submission/main_query/base_query.php
+++ classes/PDb_submission/main_query/base_query.php
@@ -93,7 +93,7 @@ abstract class base_query {
   {
     $this->post = $post;
     $this->record_id = $record_id;
-    $this->is_import = isset( $_POST[ 'csv_file_upload' ] );
+    $this->is_import = self::is_csv_import();
     $this->is_func_call = $func_call;
   }
 
@@ -117,6 +117,16 @@ abstract class base_query {
    * @return string
    */
   abstract protected function query_mode();
+  
+  /**
+   * tells if the current request is for a CSV import
+   * 
+   * @return bool
+   */
+  public static function is_csv_import()
+  {
+    return isset( $_POST[ 'csv_file_upload' ] );
+  }
 
   /**
    * provides the current query mode
