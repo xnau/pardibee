d01d514a14dc82ef4196d62d95c20f9a8d89ac09
roland <webdesign@xnau.com>
2021-12-01 07:52:08 -1000

    dynamic db fields now updated on CSV import #2692

--- classes/PDb_fields/dynamic_db_field.php
+++ classes/PDb_fields/dynamic_db_field.php
@@ -42,10 +42,14 @@ abstract class dynamic_db_field extends core {
     add_filter( 'pdb-before_submit_add', array( $this, 'update_db_value' ) );
 
     add_filter( 'pdb-update_field_def', array( $this, 'maybe_update_database' ), 10, 2 );
-    
+
     add_action( 'admin_enqueue_scripts', array( $this, 'admin_update_all' ) );
-    
+
     add_filter( 'pdb-before_list_admin_with_selected_action', array( $this, 'mass_edit_update' ), 10, 2 );
+
+    if ( !has_action( 'pdb-after_import_record', array( $this, 'process_imported_record' ) ) ) {
+      add_action( 'pdb-after_import_record', array( $this, 'process_imported_record' ), 10, 3 );
+    }
   }
 
   /**
@@ -72,7 +76,7 @@ abstract class dynamic_db_field extends core {
         $field = new \PDb_Field_Item( $dynamic_db_field );
         $field->set_record_id( $post[ 'id' ] );
         $this->setup_field( $field );
-        
+
         $this->template = new calc_template( $field, $this->default_format_tag() );
 
         $post[ $dynamic_db_field->name() ] = $this->dynamic_value( $post );
@@ -156,15 +160,14 @@ abstract class dynamic_db_field extends core {
   public function maybe_update_database( $field_data, $info )
   {
     if ( $field_data[ 'form_element' ] === $this->name ) {
-      
+
       $field_def = \Participants_Db::get_field_atts( $info[ 'name' ] );
       /** @var \PDb_Form_Field_Def $field_def */
-
       if ( $field_def->default_value() !== $field_data[ 'default' ] ) {
-      
-        $this->set_field($field_def);
+
+        $this->set_field( $field_def );
         $this->field->default = $field_data[ 'default' ];
-    
+
         // do the update
         $this->update_all_records();
       }
@@ -192,19 +195,19 @@ abstract class dynamic_db_field extends core {
   protected function update_record_list( $record_id_list = false )
   {
     global $wpdb;
-    
+
     $where = '';
-    
+
     if ( is_array( $record_id_list ) ) {
       $where = ' WHERE p.id IN ("' . implode( '","', $record_id_list ) . '")';
     }
 
     $record_list = $wpdb->get_results( 'SELECT p.id, p.' . $this->field->name() . ' FROM ' . \Participants_Db::$participants_table . ' p ' . $where . ' ORDER BY p.id ASC' );
-        
+
     status_header( 200 );
 
     foreach ( $record_list as $record ) {
-    
+
       $packet = (object) array( 'record_id' => $record->id, 'field' => $this->field->name(), 'default' => $this->field->default_value() );
       $this->process->push_to_queue( $packet );
     }
@@ -225,18 +228,18 @@ abstract class dynamic_db_field extends core {
     $this->field->set_record_id( $packet->record_id );
     $this->field->default = $packet->default;
     $value = $this->dynamic_value();
-    
+
     if ( $value === '' ) {
       $value = null;
     }
-    
+
     if ( $value !== false ) {
       $wpdb->update( \Participants_Db::participants_table(), array( $this->field->name() => $value ), array( 'id' => $packet->record_id ) );
-    
-      \Participants_Db::debug_log( __METHOD__.' query: '.$wpdb->last_query, 3 );
+
+      \Participants_Db::debug_log( __METHOD__ . ' query: ' . $wpdb->last_query, 3 );
     }
   }
-  
+
   /**
    * forces an administrative regeneration of dynamic field values
    * 
@@ -247,18 +250,17 @@ abstract class dynamic_db_field extends core {
   public function admin_update_all( $hook )
   {
     if ( strpos( $hook, 'participants-database' ) !== false && array_key_exists( 'pdb-regenerate-dynamic-fields', $_GET ) ) {
-      
-      $rebuild_field = filter_input(INPUT_GET, 'pdb-regenerate-dynamic-fields', FILTER_SANITIZE_STRING. FILTER_NULL_ON_FAILURE );
-      $field_list = \PDb_Form_Field_Def::is_field( $rebuild_field )? array( new \PDb_Form_Field_Def( $rebuild_field ) ) : $this->field_list();
-      
-      foreach( $field_list as $field ) {
+
+      $rebuild_field = filter_input( INPUT_GET, 'pdb-regenerate-dynamic-fields', FILTER_SANITIZE_STRING . FILTER_NULL_ON_FAILURE );
+      $field_list = \PDb_Form_Field_Def::is_field( $rebuild_field ) ? array( new \PDb_Form_Field_Def( $rebuild_field ) ) : $this->field_list();
+
+      foreach ( $field_list as $field ) {
         $this->set_field( $field->name() );
         $this->update_all_records();
       }
-      
     }
   }
-  
+
   /**
    * checks for the need to update when performing a mass edit on the admin list
    * 
@@ -268,25 +270,54 @@ abstract class dynamic_db_field extends core {
    */
   public function mass_edit_update( $selected_ids, $action )
   {
-    $updatable_actions = \Participants_Db::apply_filters('dynamic_field_updates_enabled_with_selected_actions', array( 'mass_edit' ) );
-    
-    if ( in_array( $action, $updatable_actions ) && count( $selected_ids['pid'] ) > 0 ) {
-      
-      $id_list = $selected_ids['pid'];
-      
+    $updatable_actions = \Participants_Db::apply_filters( 'dynamic_field_updates_enabled_with_selected_actions', array( 'mass_edit' ) );
+
+    if ( in_array( $action, $updatable_actions ) && count( $selected_ids[ 'pid' ] ) > 0 ) {
+
+      $id_list = $selected_ids[ 'pid' ];
+
       add_action( 'pdb-admin_list_with_selected_complete', function () use ( $id_list ) {
-      
-        foreach( $this->field_list() as $field ) {
+
+        foreach ( $this->field_list() as $field ) {
           $this->set_field( $field->name() );
           $this->update_record_list( $id_list );
         }
-        
-      });
+      } );
     }
-    
+
     return $selected_ids;
   }
-  
+
+  /**
+   * processes an imported record
+   * 
+   * called on the pdb-after_import_record action
+   * 
+   * @param array $post the saved data
+   * @param int $record_id the record id
+   * @param string $status the insert status for the record
+   */
+  public function process_imported_record( $post, $record_id, $status )
+  {
+    $data = array();
+
+    foreach ( $this->field_list() as $dynamic_db_field ) {
+
+      /** @var \PDb_Form_Field_Def $dynamic_db_field */
+      $field = new \PDb_Field_Item( $dynamic_db_field );
+      $field->set_record_id( $record_id );
+      $this->setup_field( $field );
+
+      $this->template = new calc_template( $field, $this->default_format_tag() );
+
+      $data[ $dynamic_db_field->name() ] = $this->dynamic_value( $post );
+    }
+    
+    if ( count( $data ) ) {
+      \Participants_Db::write_participant( $data, $record_id );
+    }
+  }
+
   /**
    * provides the datatype for the column
    * 
@@ -297,22 +328,22 @@ abstract class dynamic_db_field extends core {
   protected function field_datatype( $fieldname )
   {
     $types = $this->column_datatypes();
-    
+
     switch ( true ) {
-      
-      case stripos( $types[$fieldname], 'decimal' ) !== false:
-      case stripos( $types[$fieldname], 'int' ) !== false:
+
+      case stripos( $types[ $fieldname ], 'decimal' ) !== false:
+      case stripos( $types[ $fieldname ], 'int' ) !== false:
         return 'numeric';
-        
-      case stripos( $types[$fieldname], 'double' ) !== false:
-      case stripos( $types[$fieldname], 'float' ) !== false:
+
+      case stripos( $types[ $fieldname ], 'double' ) !== false:
+      case stripos( $types[ $fieldname ], 'float' ) !== false:
         return 'float';
-        
+
       default:
         return 'string';
     }
   }
-  
+
   /**
    * provides the main db table column datatypes
    * 
@@ -323,30 +354,28 @@ abstract class dynamic_db_field extends core {
     $cachekey = 'pdb-column-datatypes';
     $table = \Participants_Db::participants_table();
     $types = wp_cache_get( $table, $cachekey );
-    
+
     if ( $types === false ) {
-      
+
       global $wpdb;
-      
+
       $sql = 'SELECT COLUMN_NAME , DATA_TYPE
 FROM INFORMATION_SCHEMA.COLUMNS 
 WHERE 
     TABLE_SCHEMA = Database()
 AND TABLE_NAME = %s';
-      
+
       $columns = $wpdb->get_results( $wpdb->prepare( $sql, $table ), ARRAY_A );
-      
+
       $types = array();
-      foreach( $columns as $info ) {
-        $types[$info['COLUMN_NAME']] = $info['DATA_TYPE'];
+      foreach ( $columns as $info ) {
+        $types[ $info[ 'COLUMN_NAME' ] ] = $info[ 'DATA_TYPE' ];
       }
-      
+
       wp_cache_add( $table, $types, $cachekey, DAY_IN_SECONDS );
     }
-    
+
     return $types;
   }
 
 }
-
-
