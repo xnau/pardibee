3fd43bf168a1b4f5e82752cadbf9694426ecdda9
roland <webdesign@xnau.com>
2021-12-18 09:25:13 -1000

    added new classes to handle record and uploaded file deletion #2712 #2715

---
+++ classes/PDb_admin_list/delete.php
@@ -0,0 +1,144 @@
+<?php
+
+/**
+ * handles deleting records
+ *
+ * @package    WordPress
+ * @subpackage Participants Database Plugin
+ * @author     Roland Barker <webdesign@xnau.com>
+ * @copyright  2021  xnau webdesign
+ * @license    GPL3
+ * @version    0.1
+ * @link       http://xnau.com/wordpress-plugins/
+ * @depends    
+ */
+
+namespace PDb_admin_list;
+
+class delete {
+  
+  /**
+   * @param string name of the file delete preference
+   */
+  const file_delete_preference = 'delete_files';
+  
+  /**
+   * @param string name of the GET var to initiate a orphan file delete
+   */
+  const orphan_delete = 'pdb-delete_orphan_files';
+  
+  /**
+   * 
+   */
+  public function __construct()
+  {
+    add_filter( 'pdb-admin_list_with_selected_control_html', array( $this, 'add_preference_control' ) );
+    
+    global $pagenow;
+    
+    if ( $pagenow === 'admin.php' && strpos( filter_input( INPUT_GET, 'page', FILTER_SANITIZE_STRING ), 'participants-database' ) !== false ) {
+      $delete_orphan_file_types = filter_input( INPUT_GET, self::orphan_delete, FILTER_SANITIZE_STRING, FILTER_NULL_ON_FAILURE );
+      if ( $delete_orphan_file_types ) {
+        
+        $count = uploaded_files::delete_orphaned_files( $delete_orphan_file_types );
+        
+        \Participants_Db::set_admin_message( sprintf( __( 'Orphan uploaded files cleared. %s files deleted', 'participants-database' ), $count ), 'updated' );
+      }
+    }
+  }
+  
+  /**
+   * deletes a list of records
+   * 
+   * @param array $id_list
+   * @param bool $delete_files if true, delete all associated files
+   * @return string the db query that was used
+   */
+  public static function delete_records( $id_list, $delete_files = false )
+  {
+    /**
+     * @version 1.6.3
+     * @filter  pdb-before_admin_delete_record
+     * @param array $selected_ids list of ids to delete
+     */
+    $id_list = \Participants_Db::apply_filters( 'before_admin_delete_record', $id_list );
+
+    do_action( 'pdb-list_admin_with_selected_delete', $id_list );
+    
+    if ( $delete_files ) {
+      uploaded_files::delete_record_files($id_list);
+    }
+    
+    return self::_delete($id_list);
+  }
+
+  /**
+   * handles record deletion
+   * 
+   * @global \wpdb $wpdb
+   * @param array $id_list
+   * 
+   * @return string the db query that was used
+   */
+  private static function _delete( $id_list )
+  {
+    global $wpdb;
+
+    $pattern = count( $id_list ) > 1 ? 'IN ( ' . trim( str_repeat( '%s,', count( $id_list ) ), ',' ) . ' )' : '= %s';
+    $sql = "DELETE FROM " . \Participants_Db::$participants_table . " WHERE id " . $pattern;
+    $result = $wpdb->query( $wpdb->prepare( $sql, $id_list ) );
+    $last_query = $wpdb->last_query;
+
+    if ( $result > 0 ) {
+      \Participants_Db::set_admin_message( __( 'Record delete successful.', 'participants-database' ), 'updated' );
+    }
+    
+    return $last_query;
+  }
+
+  /**
+   * adds the edit controllers to the with selected control
+   * 
+   * @param array $html
+   * @return array
+   */
+  public function add_preference_control( $html )
+  {
+    foreach ( $html as $index => $row ) {
+      if ( strpos( $row, 'type="submit"' ) !== false ) {
+        break;
+      }
+    }
+
+    array_splice( $html, $index, 0, $this->preference_control() );
+
+    return $html;
+  }
+  
+  /**
+   * provides the preference control
+   * 
+   * @return string HTML
+   */
+  private function preference_control()
+  {
+    $config = array(
+        'type' => 'checkbox',
+        'name' => self::file_delete_preference,
+        'value' => $this->file_delete_preference(),
+        'options' => array( __( 'delete uploaded files with record?', 'participants-database' ) => '1', '0' => '0' ),
+    );
+    
+    return '<span class="file-delete-preference-selector" style="display:none">' . \PDb_FormElement::get_element($config) . '</span>';
+  }
+  
+  /**
+   * provides the file delete preference
+   * 
+   * @return string
+   */
+  private function file_delete_preference()
+  {
+    return \PDb_List_Admin::get_admin_user_setting( self::file_delete_preference, '0' );
+  }
+}
---
+++ classes/PDb_admin_list/uploaded_files.php
@@ -0,0 +1,238 @@
+<?php
+
+/**
+ * handles interacting with files associated with records
+ *
+ * @package    WordPress
+ * @subpackage Participants Database Plugin
+ * @author     Roland Barker <webdesign@xnau.com>
+ * @copyright  2021  xnau webdesign
+ * @license    GPL3
+ * @version    0.1
+ * @link       http://xnau.com/wordpress-plugins/
+ * @depends    
+ */
+
+namespace PDb_admin_list;
+
+class uploaded_files {
+
+  /**
+   * @var array of file names to delete
+   */
+  private $file_list;
+  
+  /**
+   * @var array list of filtered file types
+   */
+  private $type_filter;
+  
+  /**
+   * deletes the files associated with a list of record ids
+   * 
+   * @param array $record_ids
+   */
+  public static function delete_record_files( $record_ids )
+  {
+    $delete_files = new self();
+    
+    $delete_files->set_record_file_list( $record_ids );
+    $delete_files->perform_delete();
+  }
+  
+  /**
+   * deletes orphaned files in the Participants Database uploads directory
+   * 
+   * @param string $type_filter
+   * @return int number of files deleted
+   */
+  public static function delete_orphaned_files( $type_filter = '' )
+  {
+    $delete_files = new self();
+    
+    $delete_files->setup_type_filter($type_filter);
+    
+    return $delete_files->_delete_orphaned_files();
+  }
+
+  /**
+   * 
+   */
+  private function __construct()
+  {
+    
+  }
+
+  /**
+   * deletes all uploaded files for the record
+   */
+  private function perform_delete()
+  {
+    foreach( $this->file_list as $filename ) {
+      
+      if ( empty( $filename ) ) {
+        continue;
+      }
+      
+      $filepath = $this->pdb_uploads_directory_path() . $filename;  
+      
+      if ( is_file( $filepath ) ) {
+        
+        \Participants_Db::debug_log(' On record delete, deleting file: '. $filepath, 1 );
+        unlink( $filepath );
+      }
+    }
+  }
+
+  /**
+   * provides a list of upload fields
+   * 
+   * @global \wpdb $wpdb
+   * @return array of field names
+   */
+  private static function upload_fields()
+  {
+    global $wpdb;
+
+    $sql = '
+      SELECT f.name 
+      FROM ' . \Participants_Db::$fields_table . ' f 
+        JOIN ' . \Participants_Db::$groups_table . ' g 
+          ON f.group = g.name
+        WHERE f.form_element IN ("image-upload","file-upload") AND g.mode IN ("admin","private","public")';
+
+    return $wpdb->get_col( $sql );
+  }
+
+  /**
+   * provides an array of record uploaded file names
+   * 
+   * @global \wpdb $wpdb
+   * @param array $record_ids
+   */
+  private function set_record_file_list( $record_ids )
+  {
+    $file_list = array();
+    global $wpdb;
+
+    $sql = '
+      SELECT p.' . implode( ', p.', self::upload_fields() ) . ' 
+      FROM ' . \Participants_Db::$participants_table . ' p 
+        WHERE p.id IN (' . implode( ', ', array_fill( 0, count( $record_ids ), '%s' ) ) . ')';
+    
+    $result = $wpdb->get_results( $wpdb->prepare( $sql, $record_ids ), ARRAY_N );
+    
+    foreach( $result as $record ) {
+      $file_list = array_merge( $file_list, array_filter( array_values($record) ) );
+    }
+    
+    $this->file_list = $file_list;
+  }
+  
+  /**
+   * delets all files that are not attached to a record
+   * 
+   * @return int number of files deleted
+   */
+  private function _delete_orphaned_files()
+  {
+    $attached_files = $this->db_file_list();
+    $tally = 0;
+    
+    foreach( $this->uploaded_file_list() as $filename ) {
+      
+      if ( ! in_array( $filename, $attached_files ) ) {
+        
+        $filepath = $this->pdb_uploads_directory_path() . $filename;
+        
+        if ( is_file( $filepath ) ) {
+          $deleted = unlink( $filepath );
+          $tally += ( $deleted ? 1 : 0 ); 
+          \Participants_Db::debug_log(__METHOD__.' deleting: ' . $filepath );
+        }
+      }
+    }
+    
+    return $tally;
+  }
+  
+  /**
+   * provides a list of files found in the database
+   * 
+   * @global \wpdb $wpdb
+   * @return array of filenames
+   */
+  private function db_file_list()
+  {
+    global $wpdb;
+    
+    $sql = 'SELECT p.' . implode( ', p.', self::upload_fields() ) . ' 
+      FROM ' . \Participants_Db::$participants_table . ' p
+        WHERE CONCAT_WS( "|", p.' . implode( ', p.', self::upload_fields() ) . ') <> ""';
+    
+    $result = $wpdb->get_results($sql, ARRAY_N );
+    
+    $file_list = array();
+    
+    foreach ( $result as $record ) {
+      
+      $file_list = array_merge( $file_list, array_filter( $record ) );
+    }
+    
+    return $file_list;
+  }
+  
+  /**
+   * supplies the list of all uploaded files
+   * 
+   * this is filtered by the type_filter property
+   * 
+   * @return array of filenames
+   */
+  private function uploaded_file_list()
+  {
+    $raw_scan = scandir( $this->pdb_uploads_directory_path() );
+    
+    if ( ! empty( $this->type_filter ) ) {
+      
+      $file_list = array();
+      
+      foreach( $this->type_filter as $ext ) {
+        
+        $filtered_list = array_filter( $raw_scan, function($v) use ($ext) {
+          return strpos( $v, '.' . $ext ) !== false;
+        });
+        
+        $file_list = array_merge( $file_list, $filtered_list );
+      }
+      
+    } else {
+      
+      // if we get here all unattached files will be deleted
+      $file_list = $raw_scan;
+    }
+    
+    return $file_list;
+  }
+  
+  /**
+   * sets up the type filter property
+   * 
+   * @param string $type_filer
+   */
+  private function setup_type_filter( $type_filter )
+  {
+    $this->type_filter = explode( '|', $type_filter );
+  }
+  
+  /**
+   * provides the PArticipants Database uploads directory path
+   * 
+   * @return string path
+   */
+  private function pdb_uploads_directory_path()
+  {
+    return trailingslashit( \Participants_Db::files_path() );
+  }
+
+}
