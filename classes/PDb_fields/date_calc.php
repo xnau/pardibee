e97c61c8f96d5b2ce7481277d9f5bfd6591903ed
roland <webdesign@xnau.com>
2022-04-10 07:15:08 -1000

    now using the field_object method to get the field object #2814 added the use of the pdb-{$fieldname}_replacement_data filter

--- classes/PDb_fields/date_calc.php
+++ classes/PDb_fields/date_calc.php
@@ -53,7 +53,8 @@ class date_calc extends calculated_field {
     // iterate through the fields named in the template
     foreach ( $this->template_field_list() as $fieldname ) {
 
-      $template_field = new \PDb_Field_Item( array( 'name' => $fieldname, 'module' => 'list' ), $this->field->record_id() );
+      $template_field = $this->field_object( $fieldname, $this->field->record_id );
+      $template_field->set_module('list');
 
       if ( isset( $post[ $template_field->name() ] ) && !\PDb_FormElement::is_empty( $post[ $template_field->name() ] ) ) {
         // get the value from the provided data
@@ -72,8 +73,8 @@ class date_calc extends calculated_field {
     $this->calculate_value( $this->filter_data( $replacement_data ) );
 
     $replacement_data[ self::calc_tag ] = $this->result;
-
-    return $replacement_data;
+    
+    return \Participants_Db::apply_filters( $this->name . '_replacement_data', $replacement_data, $this->field );
   }
 
   /**
