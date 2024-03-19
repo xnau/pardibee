f7b8fdb16cd6ff46cff7ca24f9a17b951ee3874a
roland <webdesign@xnau.com>
2024-03-19 10:57:37 -1000

    added group name to the debug log writes #3077

--- classes/PDb_fields/string_combine.php
+++ classes/PDb_fields/string_combine.php
@@ -65,7 +65,7 @@ class string_combine extends calculated_field {
     
     $replacement_data = $this->replacement_data( $data );
     
-    \Participants_Db::debug_log(__METHOD__.' field: '. $this->field->name() .' replacement data: '. print_r($replacement_data,1), 2 );
+    \Participants_Db::debug_log(__METHOD__.' field: '. $this->field->name() .' replacement data: '. print_r($replacement_data,1), 2, 'calcfield' );
     
     $replaced_string = \PDb_Tag_Template::replace_text( $this->template(), $replacement_data );
     
