1ff4aedd55798cfe01dc7d8cc3c2a4a39f88b03f
roland <webdesign@xnau.com>
2025-03-29 07:15:47 -1000

    added context label to the write_participant call in the REST API add record write_participant call #3166

--- classes/PDb_submission/rest_api/post/add_record.php
+++ classes/PDb_submission/rest_api/post/add_record.php
@@ -29,7 +29,7 @@ class add_record extends \PDb_submission\rest_api\post_request {
    */
   protected function response()
   {
-    $result = \Participants_Db::write_participant( $this->filtered_data() );
+    $result = \Participants_Db::write_participant( $this->filtered_data(), '', 'REST API add record' );
     
     $validation = \Participants_Db::$validation_errors;
     /** @var \PDb_FormValidation $validation */
