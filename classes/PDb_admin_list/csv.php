7d707608da7fc5dfedd72b59bae2cbf40505f91a
roland <webdesign@xnau.com>
2025-04-08 07:50:08 -1000

    added support for the blank overwrite preference #3181 refactored the csv::csv_options method to better provide a complete list of options

--- classes/PDb_admin_list/csv.php
+++ classes/PDb_admin_list/csv.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * handles global CSV functionality
+ * handles global CSV preferences
  *
  * @package    WordPress
  * @subpackage Participants Database Plugin
  * @author     Roland Barker <webdesign@xnau.com>
  * @copyright  2021  xnau webdesign
  * @license    GPL3
- * @version    0.2
+ * @version    1.0
  * @link       http://xnau.com/wordpress-plugins/
  * @depends    
  */
@@ -20,6 +20,21 @@ use \Participants_Db;
 defined( 'ABSPATH' ) || exit;
 
 class csv {
+  
+  /**
+   * updates a single CSV option
+   * 
+   * @param string $option_name
+   * @param string|int $option_value
+   */
+  public static function set_csv_option( $option_name, $option_value )
+  {
+    $csv_options = self::csv_options();
+    
+    $csv_options[$option_name] = $option_value;
+    
+    self::update_option( $csv_options );
+  }
   
   /**
    * updates the csv options
@@ -38,23 +53,28 @@ class csv {
    */
   public static function csv_options()
   {
-    $csv_paramdefaults = array(
+    $csv_paramdefaults = [
         'delimiter_character' => 'auto',
         'enclosure_character' => 'auto',
         'match_field' => Participants_Db::plugin_setting( 'unique_field' ),
-        'match_preference' => Participants_Db::plugin_setting( 'unique_email' )
-    );
+        'match_preference' => Participants_Db::plugin_setting( 'unique_email' ),
+        'blank_overwrite' => Participants_Db::apply_filters( 'allow_imported_empty_value_overwrite', 0 ),
+    ];
 
     $csv_options = get_option( Participants_Db::$prefix . 'csv_import_params', $csv_paramdefaults );
     
+    $all_options = [];
+    
     // make sure these are not empty values #2669
-    foreach( array( 'delimiter_character', 'enclosure_character' ) as $param ) {
-      if ( empty( $csv_options[$param] ) ) {
-        $csv_options[$param] = $csv_paramdefaults[$param];
+    foreach( $csv_paramdefaults as $param_name => $param_value ) {
+      if ( array_key_exists( $param_name, $csv_options ) && $csv_options[$param_name] !== '' ) {
+        $all_options[$param_name] = $csv_options[$param_name];
+      } else {
+        $all_options[$param_name] = $param_value;
       }
     }
     
-    return $csv_options;
+    return $all_options;
   }
 
   /**
