fab09894615aa84354fbedcd1fc2d643af32f872
roland <webdesign@xnau.com>
2025-07-13 10:45:08 -1000

    whitespace

--- classes/PDb_Form_Field_Def.php
+++ classes/PDb_Form_Field_Def.php
@@ -511,6 +511,7 @@ class PDb_Form_Field_Def {
   public function form_element_title()
   {
     $types = PDb_FormElement::get_types();
+    
     return  isset( $types[$this->form_element] ) ? $types[$this->form_element] : $this->form_element;
   }
 
